f12578175e7fa2d5b32475dd09da7f97eca50547
rockpell <aythffk@gmail.com>
2021-01-15 16:12:23 +0900

    [Refactor] norm pipe.c
    norm에 맞춰서 함수 분할

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -1,5 +1,35 @@
 #include "mini.h"
 
+int		piping_parent(t_parse *pars, t_list *pipe_lst, int io[2])
+{
+	int		result;
+
+	if (pipe_lst->next)
+	{
+		close(io[0]);
+		dup2(io[1], 1);
+		close(io[1]);
+	}
+	result = excute_cmd(pars, pipe_lst);
+	dup2(get_info()->std[1], 1);
+	dup2(get_info()->std[0], 0);
+	if (pipe_lst->next)
+		wait(&(get_info()->exit_status));
+	return (result);
+}
+
+void	piping_chlid(t_parse *pars, t_list *pipe_lst, int io[2])
+{
+	set_process_name("pipe");
+	close(io[1]);
+	dup2(io[0], 0);
+	close(io[0]);
+	piping(pars, pipe_lst->next);
+	if (pipe_lst->next && !(pipe_lst->next->next))
+		ft_sleep(1);
+	exit(get_info()->exit_status >> 8);
+}
+
 int		piping(t_parse *pars, t_list *pipe_lst)
 {
 	int		io[2];
@@ -14,30 +44,9 @@ int		piping(t_parse *pars, t_list *pipe_lst)
 		pid = fork();
 	}
 	if (pid > 0)
-	{
-		if (pipe_lst->next)
-		{
-			close(io[0]);
-			dup2(io[1], 1);
-			close(io[1]);
-		}
-		result = excute_cmd(pars, pipe_lst);
-		dup2(get_info()->std[1], 1);
-		dup2(get_info()->std[0], 0);
-		if (pipe_lst->next)
-			wait(&(get_info()->exit_status));
-	}
+		result = piping_parent(pars, pipe_lst, io);
 	else if (pid == 0)
-	{
-		set_process_name("pipe");
-		close(io[1]);
-		dup2(io[0], 0);
-		close(io[0]);
-		piping(pars, pipe_lst->next);
-		if (pipe_lst->next && !(pipe_lst->next->next))
-			ft_sleep(1);
-		exit(get_info()->exit_status >> 8);
-	}
+		piping_chlid(pars, pipe_lst, io);
 	else
 		result = print_error(FORK_ERR, NULL);
 	return (result);
