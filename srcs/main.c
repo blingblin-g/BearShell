7749d5649d21b7681fa7958953972922a1f562df
rockpell <aythffk@gmail.com>
2021-01-16 19:12:47 +0900

    [Refactor] main

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,46 @@
 #include <string.h>
 #include "mini.h"
 
+void	piping_iter(t_parse *pars, t_pro *pro_lst)
+{
+	while (pro_lst)
+	{
+		if (piping(pars, pro_lst->pipe_lst) == ERROR)
+			break ;
+		pro_lst = pro_lst->next;
+	}
+}
+
+int		minishell_iter(t_parse *pars)
+{
+	char	*command;
+	t_pro	*pro_lst;
+
+	command = NULL;
+	set_process_name(NAME);
+	print_prompt();
+	if (check_eof(get_next_line(0, &command), &command) == ERROR)
+		return (ERROR);
+	init_pars(pars);
+	if (is_valid_line(&command) == ERROR)
+		return (ERROR);
+	if (main_parse(command, pars) == ERROR)
+	{
+		free_parse(pars, command);
+		print_error(PARSING_ERR, NULL);
+		return (ERROR);
+	}
+	pro_lst = pars->pro_lst;
+	get_info()->std[0] = dup(0);
+	get_info()->std[1] = dup(1);
+	piping_iter(pars, pro_lst);
+	free_parse(pars, command);
+	return (SUCCESS);
+}
+
 int		main(int argc, char *argv[], char **environ)
 {
 	t_parse	pars;
-	char	*command = NULL;
-	t_pro	*pro_lst;
 
 	(void)argc;
 	(void)argv;
@@ -15,29 +50,8 @@ int		main(int argc, char *argv[], char **environ)
 	signal(SIGQUIT, interrupt_handler);
 	while (MINISHELL)
 	{
-		set_process_name(NAME);
-		print_prompt();
-		if (check_eof(get_next_line(0, &command), &command) == ERROR)
+		if (minishell_iter(&pars) == ERROR)
 			continue;
-		init_pars(&pars);
-		if (is_valid_line(&command) == ERROR)
-			continue;
-		if (main_parse(command, &pars) == ERROR)
-		{
-			free_parse(&pars, command);
-			print_error(PARSING_ERR, NULL);
-			continue;
-		}
-		pro_lst = pars.pro_lst;
-		get_info()->std[0] = dup(0);
-		get_info()->std[1] = dup(1);
-		while (pro_lst)
-		{
-			if (piping(&pars, pro_lst->pipe_lst) == ERROR)
-				break ;
-			pro_lst = pro_lst->next;
-		}
-		free_parse(&pars, command);
 	}
 	return (get_info()->exit_status);
 }
