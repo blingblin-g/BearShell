29c7f390bf44d397c6e0c02c16ac3d596db363eb
rockpell <aythffk@gmail.com>
2021-01-16 22:48:51 +0900

    [Refactor] norm mini.h

--- include/mini.h
+++ include/mini.h
@@ -6,112 +6,113 @@
 /*   By: sooyoon <sooyoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 22:30:37 by sooyoon           #+#    #+#             */
-/*   Updated: 2021/01/16 03:46:26 by sooyoon          ###   ########.fr       */
+/*   Updated: 2021/01/16 22:48:39 by chlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_H
-#define MINI_H
+# define MINI_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include "libft.h"
-#include <dirent.h>
-#include <fcntl.h>
-#include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
+# include "libft.h"
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
 
-#define MINISHELL 42
-#define NAME "minishell"
-
-/*
-* boolean
-*/
-
-#define FALSE 0
-#define TRUE 1
+# define MINISHELL 42
+# define NAME "minishell"
 
 /*
-* need to add error
+** boolean
 */
 
-#define ERROR 0
-#define NOT_FOUND -1
-#define SUCCESS 1
-#define NOT_BUILTIN 2
-
-#define PIPE 1
-#define INPUT 2
-#define OUTPUT 3
-#define APPEND 4
-#define SEMI 5
-#define SPACE 6
-#define END 7
-
-#define FORK_ERR 1
-#define PATH_ERR 2
-#define PARSING_ERR 3
-#define COMMAND_ERR 4
-
-typedef struct dirent t_dir;
-
-typedef struct s_info
-{
-	t_list	*env_list;
-	char	*process_name;
-	int		process_index;
-	int		exit_status;
-	int		pid;
-	int		ppid;
-	int		is_minishell;
-	int		std[2];
-} t_info;
+# define FALSE 0
+# define TRUE 1
 
 /*
-* cmd_lst : command list
-* pro_lst : process list
+** need to add error
 */
 
-typedef struct	s_exec
+# define ERROR 0
+# define NOT_FOUND -1
+# define SUCCESS 1
+# define NOT_BUILTIN 2
+
+# define PIPE 1
+# define INPUT 2
+# define OUTPUT 3
+# define APPEND 4
+# define SEMI 5
+# define SPACE 6
+# define END 7
+
+# define FORK_ERR 1
+# define PATH_ERR 2
+# define PARSING_ERR 3
+# define COMMAND_ERR 4
+
+struct	s_info
 {
-	int			*fd[2]; // fd 이차원 배열, fd[0]은 input 대상이이 들어온다. fd[1]은 output 대상이 들어온다., open으로 하나씩 열어놔야함
-	char		**argv; // 인자 배열
-	int			fd_count;
-	int			fd_input_idx;
-	int			fd_output_idx;
-	int			std[2];
-	int			argv_idx;
-	int			input_count;
-	int			output_count;
-}				t_exec;
+	t_list			*env_list;
+	char			*process_name;
+	int				process_index;
+	int				exit_status;
+	int				pid;
+	int				ppid;
+	int				is_minishell;
+	int				std[2];
+};
+typedef struct s_info	t_info;
 
-typedef struct	s_pro
+/*
+** cmd_lst : command list
+** pro_lst : process list
+*/
+
+struct	s_exec
 {
-	int			type;
-	char		*raw;
-	t_list		*pipe_lst;
-	struct		s_pro *next;
-}				t_pro;
+	int				*fd[2];
+	char			**argv;
+	int				fd_count;
+	int				fd_input_idx;
+	int				fd_output_idx;
+	int				std[2];
+	int				argv_idx;
+	int				input_count;
+	int				output_count;
+};
+typedef struct s_exec	t_exec;
 
-typedef struct	s_parse
+struct	s_pro
 {
-	int			is_space;
-	int			is_redirection;
-	int			single_q;
-	int			double_q;
-	int			fd_count;
-	int			type;
-	size_t		start;
-	char		*line;
-	t_pro		*pro_lst;
-}				t_parse;
+	int				type;
+	char			*raw;
+	t_list			*pipe_lst;
+	struct s_pro	*next;
+};
+typedef struct s_pro	t_pro;
 
+struct	s_parse
+{
+	int				is_space;
+	int				is_redirection;
+	int				single_q;
+	int				double_q;
+	int				fd_count;
+	int				type;
+	size_t			start;
+	char			*line;
+	t_pro			*pro_lst;
+};
+typedef struct s_parse	t_parse;
 
-/**********
-** utils **
-***********/
+/*
+** utils
+*/
 
 t_info	*get_info();
 size_t	len(char **arr);
@@ -119,10 +120,9 @@ int		find_chr(char *str, char ch);
 char	*free_join(char *s1, char *s2);
 void	ft_sleep(int amount);
 
-
-/****************
-** print_utils **
-*****************/
+/*
+** print_utils
+*/
 
 void	print_str(void *str);
 void	print_err(void *str);
@@ -130,11 +130,9 @@ void	print_arr(char **arr);
 void	println_arr(char **arr);
 void	print_lst(t_list *lst);
 
-
-
-/**************
-** built-ins **
-***************/
+/*
+** built-ins
+*/
 
 int		pwd();
 int		cd(char *argv);
@@ -142,20 +140,18 @@ int		echo(char **argv);
 int		builtin_exit(char **argv);
 void	free_exit();
 
-
-/*************
-** is_valid **
-**************/
+/*
+** is_valid
+*/
 
 int		valid_path(char *path);
 int		valid_first_command(char c);
 int		valid_pipe(t_list *pipe_lst);
 int		is_valid_line(char **line);
 
-
-/*********
-**  env **
-**********/
+/*
+**  env
+*/
 
 int		env();
 void	sort_env_arr(char **arr);
@@ -163,19 +159,17 @@ void	export_only();
 int		export(char *argv);
 int		unset(char *argv);
 
-
-/***************
-** search_env **
-****************/
+/*
+** search_env
+*/
 
 char	*get_env_item(char *key);
 t_list	*create_env_list(char **environ);
 char	**get_environ();
 
-
-/******************
-** parsing_utils **
-*******************/
+/*
+** parsing_utils
+*/
 
 int		is_semi_char(char c);
 int		is_pipe_char(char c);
@@ -183,11 +177,9 @@ int		is_redirection_char(char c);
 void	init_pars(t_parse *pars);
 int		search_quotes(char c, char *line, int i);
 
-
-
-/*******************
-** parsing_utils2 **
-********************/
+/*
+** parsing_utils2
+*/
 
 int		how2divide(char c);
 int		is_error(char c);
@@ -195,20 +187,19 @@ int		is_outofquote_end(char c);
 int		is_escape(char c);
 int		is_sayeon(char c);
 
-
-/*******************
-** parsing_utils3 **
-********************/
+/*
+** parsing_utils3
+*/
 
 void	process_sayeon(char c, char **result);
 char	*find_var_name(char *content, size_t *i);
 char	*process_home(char *content);
 void	is_quotes_true(char c, t_parse *pars);
-void	is_quotes_false(char* line, size_t i, t_parse *pars);
+void	is_quotes_false(char *line, size_t i, t_parse *pars);
 
-/******************
-** prolist_utils **
-*******************/
+/*
+** prolist_utils
+*/
 
 t_pro	*new_prolst(char *raw, int type);
 t_pro	*last_prolst(t_pro *lst);
@@ -216,168 +207,136 @@ void	add_back_prolst(t_pro **lst, t_pro *new);
 void	free_prolst(t_pro **lst);
 void	concat_prolst(t_parse *pars, char *line, size_t i, int type);
 
-
-
-/**************
-** set_items **
-***************/
+/*
+** set_items
+*/
 
 int		set_string(char **old_str, char *new_str);
 int		set_process_name(char *name);
 
-
-/************
-** get_cmd **
-*************/
+/*
+** get_cmd
+*/
 
 char	*join_cmd_envpath(char	**path, char *command);
 char	*get_cmd(char *cmd);
 
-
-
-/*********
-** free **
-**********/
+/*
+** free
+*/
 
 void	free_parse(t_parse *parse, char *command);
 void	free_arr(char **arr);
 void	free_exec_info(t_exec **exec_info);
 char	*free_strtrim(char **s, char const *set);
 
-
-
-/**************
-** lst_utils **
-***************/
+/*
+** lst_utils
+*/
 
 t_list	*new_lst_trim(char *content);
 void	add_redirection_lst(t_parse *pars, t_list **lst, int type);
 
+/*
+** exec_utils
+*/
 
-
-/****************
- ** exec_utils **
-****************/
-
-int		 init_exec(t_exec	*exec_info, int lst_count);
+int		init_exec(t_exec	*exec_info, int lst_count);
 t_exec	*create_exec(t_parse *pars, t_list *redir_lst);
 
-
-
-/********
- ** fd **
- ********/
+/*
+** fd
+*/
 
 void	get_fd_count(t_list	*redir_lst, t_exec *exec_info);
 int		create_fds(t_exec *exec_info, char *redir_str, char *file_str);
 void	close_fds(t_exec *exec_info);
 
-
-
-/****************
- ** excute_cmd **
- ****************/
+/*
+** excute_cmd
+*/
 
 int		excute_cmd(t_parse *pars, t_list *pipe_lst);
 int		execute_builtin(char **argv);
 
-
-
-/**************
- ** redirect **
- **************/
+/*
+** redirect
+*/
 
 int		input_redirection_lst(t_parse *pars, char *raw, t_list **raw_lst);
 t_exec	*redir_process(t_parse *pars, t_list *pipe_lst);
 
-
-
-/*****************
- ** redir_utils **
- *****************/
+/*
+** redir_utils
+*/
 
 int		is_redir_pipe(char ch);
 int		find_multi_redir(char *text);
 int		check_continuous(char *str);
 
-
-
-/**********
- ** pipe **
- **********/
+/*
+** pipe
+*/
 
 int		piping(t_parse *pars, t_list *pipe_lst);
 
-
-
-
-/****************
- ** main_utils **
- ****************/
+/*
+** main_utils
+*/
 
 void	print_prompt();
 void	interrupt_handler(int sig);
 int		check_eof(int gnl_value, char **command);
 
-
-
-/***********
- ** error **
- ***********/
+/*
+** error
+*/
 
 int		print_error(int type, char *path);
 int		input_pipe_lst(t_parse *pars, char *raw, t_list **raw_lst);
 
+/*
+** parse_pipe
+*/
 
-/****************
- ** parse_pipe **
- ****************/
+int		make_pipe_lst(t_pro *pro_lst, t_parse *pars);
+void	substr_addlst_back(t_list **raw_lst, char *raw, int start, int i);
 
-
-int			make_pipe_lst(t_pro *pro_lst, t_parse *pars);
-void		substr_addlst_back(t_list **raw_lst, char *raw, int start, int i);
-
-
-/************
-** parsing **
-*************/
-
+/*
+** parsing
+*/
 
 int		main_parse(char *line, t_parse *pars);
 
-
-/****************
-** quotes_zone **
-*****************/
-
+/*
+** quotes_zone
+*/
 
 char	*get_single_quote_zone(char *content, size_t *start, size_t *i);
 char	*get_double_quote_zone(char *content, size_t *start, size_t *i);
 char	*out_of_quotes_zone(char *content, size_t *start, size_t *i);
 
+/*
+** quotes_zone_util
+*/
 
-/****************
-** quotes_zone_util **
-*****************/
+void	fill_result(char **result, char *content,
+		size_t *start, size_t *i);
+void	fill_result_plus_start(char **result, char *content,
+		size_t *start, size_t *i);
+void	fill_result_plus_i(char **result, char *content,
+		size_t *start, size_t *i);
 
-
-void	fill_result(char **result, char *content, size_t *start, size_t *i);
-void	fill_result_plus_start(char **result, char *content, size_t *start, size_t *i);
-void	fill_result_plus_i(char **result, char *content, size_t *start, size_t *i);
-
-
-/****************
-** process_quotes **
-*****************/
+/*
+** process_quotes
+*/
 
 char	*process_quotes(t_parse *pars, char *content);
 
-
-/*********
-** main **
-**********/
+/*
+** main
+*/
 
 int		get_next_line(int fd, char **line);
-
 
 #endif
